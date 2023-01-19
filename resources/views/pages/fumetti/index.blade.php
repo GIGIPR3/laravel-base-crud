0a631dd007516e96faeaa65a6644c00d5486d083
GIGIPR3 <gigiprencipe47@gmail.com>
2023-01-19 17:38:49 +0100

    header, seeder, crud, migrations created!

---
+++ resources/views/pages/fumetti/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title-page', 'DC COMICS - Home')
+@section('main-content')
+
+    <h1>Lista dei fumetti</h1>
+
+@endsection
---
+++ resources/views/pages/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title-page', 'DC COMICS - Home')
+@section('main-content')
+
+    <h1>Home page</h1>
+
+@endsection
